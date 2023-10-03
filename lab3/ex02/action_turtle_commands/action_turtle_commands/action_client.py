4e69c8bbda59ec5d10a89fab69d956ceb2d0339d
fedor.tikunov@gmail.com <fedor.tikunov@gmail.com>
2023-10-03 20:32:37 +0600

    add ex02

---
+++ lab3/ex02/action_turtle_commands/action_turtle_commands/action_client.py
@@ -0,0 +1,74 @@
+from action_turtle_interfaces.action import MessageTurtleCommands
+
+import rclpy
+from rclpy.action import ActionClient
+from rclpy.node import Node
+
+tasks = []
+
+class MessageActionClient(Node):
+
+    def __init__(self):
+        super().__init__('fibonacci_action_client')
+        self._action_client = ActionClient(self, Fibonacci, 'fibonacci')
+
+    def send_goal(self, order):
+        goal_msg = MessageTurtleCommands.Goal()
+        goal_msg.s = s
+        goal_msg.angle = angle
+        goal_msg.command = command
+
+
+        self._action_client.wait_for_server()
+
+        self._send_goal_future = self._action_client.send_goal_async(
+            goal_msg,
+            feedback_callback=self.feedback_callback)
+
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+
+        self.get_logger().info('Goal accepted :)')
+
+        self._get_result_future = goal_handle.get_result_async()
+
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info('Result: {0}'.format(result.sequence))
+        global goals
+        goals.pop(0)
+        if goals:
+            self.send_goal(*tasks[0])
+        else:
+            rclpy.shutdown()
+
+    def feedback_callback(self, feedback_msg):
+        feedback = feedback_msg.feedback
+        self.get_logger().info('Received feedback: {0}'.format(feedback.partial_sequence))
+
+
+def main(args=None):
+    global tasks
+    rclpy.init(args=args)
+
+    action_client = MessageActionClient()
+    
+    tasks.append(['forward', 2, 0])
+    tasks.append(['turn_right', 0, 90])
+    tasks.append(['forward', 1, 0])
+    
+    action_client.send_goal(*goals[0])
+
+    rclpy.spin(action_client)
+
+
+if __name__ == '__main__':
+    main()
---
+++ lab3/ex02/action_turtle_commands/action_turtle_commands/action_server.py
@@ -0,0 +1,108 @@
+import rclpy
+from rclpy.action import ActionServer
+from rclpy.node import Node
+
+from action_turtle_interfaces.action import MessageTurtleCommands
+
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+from rclpy.executors import MultiThreadedExecutor
+
+curr = Pose()
+
+class MessageActionServer(Node):
+
+    def __init__(self):
+        super().__init__('action_server')
+        self._action_server = ActionServer(
+            self,
+            MessageTurtleCommands,
+            'execute_turtle_interfaces',
+            self.execute_callback)
+        self.publisher = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        
+    def execute_callback(self, goal_handle):
+        self.get_logger().info('Executing goal...')
+        
+        feedback_msg = MessageTurtleCommands.Feedback()
+        feedback_msg.odom = 0
+        
+        global curr
+
+        x, y = curr.x, curr.y
+
+        
+        twist = Twist()
+        
+        if goal_handle.request.command == 'forward':
+        	twist.linear.x = float(goal_handle.request.s)
+        	self.publisher.publish(twist)
+        	while feedback_msg.odom != goal_handle.request.s:
+        		feedback_msg.odom = int(math.sqrt((curr.x - x)**2+(curr.y - y)**2))
+        		self.get_logger().info(f'went: {curr.x} {x} m')
+        		goal_handle.publish_feedback(feedback_msg)
+
+        else:
+            if goal_handle.request.command == 'turn_right':
+                twist.angular.z = -1.0*float(goal_handle.request.angle)*2*3.14/360  # deg -> rad
+                self.publisher.publish(twist)
+                while (abs((curr.theta-t0)) < float(goal_handle.request.angle)*2*3.14/360):
+                    self.get_logger().info(f'rotated: {(curr.theta-t0)*360/6.28} degrees')
+            else: 
+                twist.angular.z = float(goal_handle.request.angle)*2*3.14/360
+                self.publisher.publish(twist)
+                while (abs((curr.theta-t0)) < float(goal_handle.request.angle)*2*3.14/360):
+                    self.get_logger().info(f'rotated: {abs(curr.theta-t0)*360/6.28} degrees')
+
+        goal_handle.succeed()
+        twist.linear.x = 0.0
+        self.publisher.publish(twist)
+        if goal_handle.request.command == 'forward':
+            self.get_logger().info(f'position: {curr.x} {curr.y} m')
+        else:
+            self.get_logger().info(f'angle: {curr.theta*360/6.28} degrees')
+        
+        result = MessageTurtleCommands.Result()
+        result.result = True
+        return result
+        
+
+class ActionSubscriber(Node):
+    """
+    Subscriber node to the current battery state
+    """     
+    def __init__(self):
+   
+      # Initialize the class using the constructor
+      super().__init__('action_subscriber')
+     
+      # Create a subscriber 
+      # This node subscribes to messages of type
+      # Pose
+      self.subscription = self.create_subscription(
+            Pose,
+            '/turtle1/pose',
+            self.listener_callback,
+            10)
+
+    def listener_callback(self, msg):
+    	global curr
+    	curr = msg
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    action_subscriber = ActionSubscriber()
+    action_server = MessageActionServer()
+    executor = MultiThreadedExecutor(num_threads=4)
+    executor.add_node(action_server) #1
+    executor.add_node(action_subscriber) #2
+    executor.spin() #3
+    executor.shutdown() #4
+    action_server.destroy_node()
+    action_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
