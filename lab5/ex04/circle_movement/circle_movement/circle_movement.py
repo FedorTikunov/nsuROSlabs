efc176d1d2139bfcd2a5035566464d5a82b29e32
fedor.tikunov@gmail.com <fedor.tikunov@gmail.com>
2023-11-05 21:35:10 +0600

    add 04

---
+++ lab5/ex04/circle_movement/circle_movement/circle_movement.py
@@ -0,0 +1,35 @@
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+from rclpy.node import Node
+import rclpy
+import sys
+import math
+import time
+
+class CirclePublisher(Node):
+
+    def __init__(self):
+        super().__init__('publisher')
+        self.publisher = self.create_publisher(Twist, '/robot/cmd_vel', 10)
+        timer_period = 0.5  
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+
+        twist = Twist()
+        twist.linear.x = 1.
+        twist.angular.z = 0.5         
+        self.publisher.publish(twist)
+        
+         
+def main(args=None):
+    rclpy.init(args=args)
+    circling = CirclePublisher()
+    rclpy.spin(circling)
+    rclpy.spin(circling)
+
+    rclpy.shutdown()
+
+ 
+if __name__ == '__main__':
+    main()
