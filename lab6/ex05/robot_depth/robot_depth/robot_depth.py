0f5747f44f88dd5f02541fbc1d98e2e787ec77e6
fedor.tikunov@gmail.com <fedor.tikunov@gmail.com>
2023-11-12 19:41:51 +0600

    fix ;)

--- lab6/ex05/robot_depth/robot_depth/robot_depth.py
+++ lab6/ex05/robot_depth/robot_depth/robot_depth.py
@@ -1,60 +1,56 @@
-import rclpy, sys
-from std_msgs.msg import String
-from rclpy.node import Node
-
-from sensor_msgs.msg import PointCloud2, PointField, Image
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Image
+from rclpy.node import Node
+import rclpy
+
+import sys
 import math
-from point_cloud2 import read_points
+import time
+class Happy(Node):
 
-from cv_bridge import CvBridge, CvBridgeError
-import cv2
-import numpy as np
+     def __init__(self):
+         # Creates a node with name 'turtlebot_controller' and make sure it is a
+         # unique node (using anonymous=True).
+         super().__init__('happy')
 
-is_obstacle = 0
+         # Publisher which will publish to the topic '/turtle1/cmd_vel'.
+         self.velocity_publisher = self.create_publisher(Twist, '/robot/cmd_vel', 10)
+         self.pose_subscriber = self.create_subscription(Image, '/depth/image', self.update_pose, 10)
+         self.scan = Image()
+         self.timer = self.create_timer(0.5, self.move)
 
+     def update_pose(self, data):
+         self.scan = data
 
-class CirclePublisher(Node):
+     def move(self):
+         """Moves the turtle to the goal."""
+         
+         vel_msg = Twist()
+         d = self.scan.data
+         vel_msg.linear.x = 0.
+         if (len(d)!=0):
+            tmp = int(d[int(self.scan.width*self.scan.height/2 + self.scan.width/2)])
+         #self.get_logger().info('%d" ' % laser)
+            if (tmp==0.0 or tmp == 127 or tmp == 128):
+                vel_msg.linear.x = 0.3
+            else:
+                vel_msg.linear.x = 0.0
+         vel_msg.angular.z = 0.0
+         self.velocity_publisher.publish(vel_msg)
 
-    def __init__(self):
-        super().__init__('publisher')
-        self.publisher = self.create_publisher(Twist, '/robot/cmd_vel', 10)
-        self.subscription = self.create_subscription(Image, "/depth/image",
-         	    self.listener_callback, 10)
-        timer_period = 0.5
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-       
-
-    def timer_callback(self):
-        twist = Twist()
-        if is_obstacle:
-            twist.linear.x = 0.
-        else:
-            twist.linear.x = 1.
-        self.publisher.publish(twist)
-        
-
-    def listener_callback(self, msg):
-        cv_bridge = CvBridge()
-        depth_image = cv_bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-        depth_array = np.array(depth_image, dtype=np.float32)
-        h, w = depth_array.shape
-        print(depth_array.shape, depth_array)
-        global is_obstacle
-        is_obstacle = 1 if depth_array[120][w//2] < 4 else 0
-        
-        
+         
+ 
+         
 def main(args=None):
     rclpy.init(args=args)
-
-    circling = CirclePublisher()
-
-    rclpy.spin(circling)
-
-    #circling.destroy_node()
+    #give time to place an obstacle 
+    time.sleep(2)
+    x = Happy()
+    rclpy.spin(x)
+    x.destroy_node()
     rclpy.shutdown()
 
-
+ 
 if __name__ == '__main__':
     main()
     
